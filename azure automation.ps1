60b8ab53a251d1f1ae6c39f00b5953a15ad46f9b
jknorrsmith <31988953+jknorrsmith@users.noreply.github.com>
2017-09-15 12:56:27 -0400

    Add files via upload

---
+++ azure automation.ps1
@@ -0,0 +1,18 @@
+﻿<#•    A diagnostic script that will 
+o    Ping the problem server 
+o    Ping all DC
+o    Ping router near side 
+o    Ping router far side 
+o    Ping the server on RDP port
+•    Create 5 recovery scripts that will 
+o    Check the space in a SQL log file and then truncate the logs if they are full
+o    Restart the W3SVC if a website has become unresponsive 
+o    Restart a service if it fails 
+o    Restart a server if the CPU is 100% for 60 min
+#>
+#Install-AzureAutomationIseAddOn
+
+
+Install-Module AzureAutomationAuthoringToolkit -Scope CurrentUser
+
+Import-Module AzureAutomationAuthoringToolkit
